a796969718f69629e5d26f343da1bfcbcdba33d2
maryovos <maryosina16@gmail.com>
2024-07-19 14:39:49 +0700

    first commit

---
+++ Test Suites/TS_Search.ts
@@ -0,0 +1,36 @@
+<?xml version="1.0" encoding="UTF-8"?>
+<TestSuiteEntity>
+   <description></description>
+   <name>TS_Search</name>
+   <tag></tag>
+   <isRerun>false</isRerun>
+   <mailRecipient></mailRecipient>
+   <numberOfRerun>3</numberOfRerun>
+   <pageLoadTimeout>30</pageLoadTimeout>
+   <pageLoadTimeoutDefault>true</pageLoadTimeoutDefault>
+   <rerunFailedTestCasesOnly>false</rerunFailedTestCasesOnly>
+   <rerunImmediately>true</rerunImmediately>
+   <testSuiteGuid>f7807990-cb53-4042-86fc-1fbfd1c2799a</testSuiteGuid>
+   <testCaseLink>
+      <guid>bcc1c51a-1e98-4519-aeac-283b09b66ffd</guid>
+      <isReuseDriver>false</isReuseDriver>
+      <isRun>true</isRun>
+      <testCaseId>Test Cases/Wikipedia/TC_Wikipedia_Search</testCaseId>
+      <testDataLink>
+         <combinationType>ONE</combinationType>
+         <id>a58f0cf1-51d6-4242-b3ea-f28fce342219</id>
+         <iterationEntity>
+            <iterationType>ALL</iterationType>
+            <value></value>
+         </iterationEntity>
+         <testDataId>Data Files/searchData</testDataId>
+      </testDataLink>
+      <usingDataBindingAtTestSuiteLevel>true</usingDataBindingAtTestSuiteLevel>
+      <variableLink>
+         <testDataLinkId>a58f0cf1-51d6-4242-b3ea-f28fce342219</testDataLinkId>
+         <type>DATA_COLUMN</type>
+         <value>search</value>
+         <variableId>8d045736-d767-475f-9cea-a1a084b6b18a</variableId>
+      </variableLink>
+   </testCaseLink>
+</TestSuiteEntity>
